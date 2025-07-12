f09ff62e192c397d53460b3a19395fb6e95b3642
prithika25-3 <prithikaprithi2007@gmail.com>
2025-07-12 10:50:05 +0530

    Add files via upload

---
+++ js day1/index.js
@@ -0,0 +1,19 @@
+var number=12;
+var name='js';
+var iscloudly=true;
+console.log(number+" "+name+" "+iscloudly);
+document.write(number+" "+name+" "+iscloudly);
+
+
+//task2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudly));
+
+//task3
+var num1=parseInt(prompt("enter the first value"));
+var num2=parseInt(prompt("enter the second value"));
+alert(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
